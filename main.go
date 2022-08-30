c95219d6a6ec9233715f29a6abebcae17344eba9
Longs Pemun Gotar <developerlongs@gmail.com>
2022-08-30 12:51:42 +0000

    added few lines

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 import "fmt"
 
 func main(){
-	var conferenceName = "Go Conference";
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference";
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceTickets datatype is %T, remainingTickets datatype is %T, conferenceName dataype is %T\n", conferenceTickets, remainingTickets, conferenceName);
 
@@ -16,7 +16,7 @@ func main(){
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 	// ask user for their name
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -30,5 +30,9 @@ func main(){
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }

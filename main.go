4f60b3a2a26d608c6b4146870e51bebbae29dcf0
Longs Pemun Gotar <developerlongs@gmail.com>
2022-08-30 12:44:30 +0000

    added user prompt

--- main.go
+++ main.go
@@ -6,8 +6,29 @@ func main(){
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
+	fmt.Printf("conferenceTickets datatype is %T, remainingTickets datatype is %T, conferenceName dataype is %T\n", conferenceTickets, remainingTickets, conferenceName);
+
 	// fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
